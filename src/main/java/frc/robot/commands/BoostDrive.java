e07ff38f1a34ef70d02a15bc4ebca28358d50dba
David Bolme <dbolme@gmail.com>
2023-02-11 10:37:08 -0500

    Initail working code with drivetrain and leds.

---
+++ src/main/java/frc/robot/commands/BoostDrive.java
@@ -0,0 +1,69 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import java.util.function.Supplier;
+
+public class BoostDrive extends CommandBase {
+  private final DriveTrain m_drivetrain;
+  private final Supplier<Double> m_xaxisSpeedSupplier;
+  private final Supplier<Double> m_zaxisRotateSupplier;
+  private final Supplier<Double> m_boostAxisSupplier;
+  private final Supplier<Double> m_slowAxisSupplier;
+
+  double speed = 0.65;
+
+  /**
+   * Creates a new ArcadeDrive. This command will drive your robot according to the speed supplier
+   * lambdas. This command does not terminate.
+   *
+   * @param drivetrain The drivetrain subsystem on which this command will run
+   * @param xaxisSpeedSupplier Lambda supplier of forward/backward speed
+   * @param zaxisRotateSupplier Lambda supplier of rotational speed
+   */
+  public BoostDrive(
+      DriveTrain drivetrain,
+      Supplier<Double> xaxisSpeedSupplier,
+      Supplier<Double> zaxisRotateSupplier,
+      Supplier<Double> boostAxisSupplier,
+      Supplier<Double> slowAxisSupplier) {
+    m_drivetrain = drivetrain;
+    m_xaxisSpeedSupplier = xaxisSpeedSupplier;
+    m_zaxisRotateSupplier = zaxisRotateSupplier;
+    m_boostAxisSupplier = boostAxisSupplier;
+    m_slowAxisSupplier = slowAxisSupplier;
+    addRequirements(drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //double fast = 0.5+0.5*m_boostAxisSupplier.get();
+    //double slow = 0.5+0.5*m_slowAxisSupplier.get();
+    double fast = m_boostAxisSupplier.get();
+    double slow = m_slowAxisSupplier.get();
+    double boost = speed + 0.35*fast - 0.35*slow;
+
+    System.out.printf("F: %.3f   S: %.3f   B: %.3f\n",fast,slow,boost);
+    
+    m_drivetrain.arcadeDrive(boost*m_xaxisSpeedSupplier.get(), boost*m_zaxisRotateSupplier.get());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
