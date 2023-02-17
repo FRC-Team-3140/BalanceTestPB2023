bfd830e397ba3d14f195df92746a363944bd4a60
Me-Smrt <code_t@krummhere.com>
2023-02-17 18:23:06 -0500

    Made Networktables Init on Robot start and Data feed on TeleOp periodic

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,16 +15,13 @@ package frc.robot;
 import edu.wpi.first.hal.FRCNetComm.tInstances;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-import com.ctre.phoenixpro.controls.VelocityDutyCycle;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.I2C.Port;
-import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -32,7 +29,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in 
+ * creating this project, you must also update the build.properties file in
  * the project.
  */
 public class Robot extends TimedRobot {
@@ -51,32 +48,41 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotInit() {
-        // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        // Instantiate our RobotContainer. This will perform all our button bindings,
+        // and put our
         // autonomous chooser on the dashboard.
         m_robotContainer = RobotContainer.getInstance();
         HAL.report(tResourceType.kResourceType_Framework, tInstances.kFramework_RobotBuilder);
+
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        DataNAVX = inst.getTable("SmartDashboard").getSubTable("DataNAVX");
     }
 
     /**
-    * This function is called every robot packet, no matter the mode. Use this for items like
-    * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-    *
-    * <p>This runs after the mode specific periodic functions, but before
-    * LiveWindow and SmartDashboard integrated updating.
-    */
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like
+     * diagnostics that you want ran during disabled, autonomous, teleoperated and
+     * test.
+     *
+     * <p>
+     * This runs after the mode specific periodic functions, but before
+     * LiveWindow and SmartDashboard integrated updating.
+     */
     @Override
     public void robotPeriodic() {
-        // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-        // commands, running already-scheduled commands, removing finished or interrupted commands,
-        // and running subsystem periodic() methods.  This must be called from the robot's periodic
+        // Runs the Scheduler. This is responsible for polling buttons, adding
+        // newly-scheduled
+        // commands, running already-scheduled commands, removing finished or
+        // interrupted commands,
+        // and running subsystem periodic() methods. This must be called from the
+        // robot's periodic
         // block in order for anything in the Command-based framework to work.
         CommandScheduler.getInstance().run();
     }
 
-
     /**
-    * This function is called once each time the robot enters Disabled mode.
-    */
+     * This function is called once each time the robot enters Disabled mode.
+     */
     @Override
     public void disabledInit() {
     }
@@ -86,8 +92,9 @@ public class Robot extends TimedRobot {
     }
 
     /**
-    * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
-    */
+     * This autonomous runs the autonomous command selected by your
+     * {@link RobotContainer} class.
+     */
     @Override
     public void autonomousInit() {
         m_autonomousCommand = m_robotContainer.getAutonomousCommand();
@@ -99,8 +106,8 @@ public class Robot extends TimedRobot {
     }
 
     /**
-    * This function is called periodically during autonomous.
-    */
+     * This function is called periodically during autonomous.
+     */
     @Override
     public void autonomousPeriodic() {
     }
@@ -114,11 +121,15 @@ public class Robot extends TimedRobot {
         if (m_autonomousCommand != null) {
             m_autonomousCommand.cancel();
         }
-        
+    }
+
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() {
         // displacement, Accelerometer, gyro, Velocity
-        NetworkTableInstance inst = NetworkTableInstance.getDefault();
-        DataNAVX = inst.getTable("DataNAVX").getSubTable("Jonathan Wasn't here...");
-        
+
         DataNAVX.getEntry("Accelerometer: x").setNumber(NAVX.getRawAccelX());
         DataNAVX.getEntry("Accelerometer: y").setNumber(NAVX.getRawAccelY());
         DataNAVX.getEntry("Accelerometer: z").setNumber(NAVX.getRawAccelZ());
@@ -133,14 +144,6 @@ public class Robot extends TimedRobot {
         DataNAVX.getEntry("Velocity: z").setNumber(NAVX.getVelocityZ());
     }
 
-    /**
-     * This function is called periodically during operator control.
-     */
-    @Override
-    public void teleopPeriodic() {
-        
-    }
-
     @Override
     public void testInit() {
         // Cancels all running commands at the start of test mode.
@@ -148,8 +151,8 @@ public class Robot extends TimedRobot {
     }
 
     /**
-    * This function is called periodically during test mode.
-    */
+     * This function is called periodically during test mode.
+     */
     @Override
     public void testPeriodic() {
     }
